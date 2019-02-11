90580db0f2a0ca198e978b588fdd13aa83c7d88d
bendou <sami.dahoux@etu.emse.fr>
2019-02-11 03:12:58 +0100

    code : integration of Planning into purchase features

--- purchase.py
+++ purchase.py
@@ -1,5 +1,4 @@
 from utilities import GenericPlot
-from model import Market
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,31 +22,18 @@ class Data:
         plan_profitability = []
         for plan in self.plannings:
             plan_profitability.append(0)
-            for day in range(0, 7):
-                plan_profitability[-1] += sum(plan.by_hubs(plan.profitability(day), day).values()) / 7.
+            plan_profitability[-1] += sum(plan.by_hubs(plan.profitability()).values())
 
         zip_to_sort = zip(plan_profitability, self.plannings)
         sorted_plans = [x for _, x in sorted(zip_to_sort, reverse=True)]
         return sorted_plans
 
-    def heatmap(self, included_planes=None, excluded_planes=None):
-        planes_names = self._filter_planes(included_planes, excluded_planes)
+    def heatmap(self):
+        plan_profitability = []
+        for plan in self.plannings:
+            plan_profitability.append(plan.by_lines(plan.profitability()))
 
-        planes = {}
-        for iata, h in self.lines.items():
-            planes[iata] = {}
-            for l in h.values():
-                planes[iata][l.dst.iata] = list(
-                    filter(lambda x: True if x.name in planes_names else False, self.planes.values()))
-
-        return planes
-
-    def _filter_planes(self, included_planes, excluded_planes):
-        inc = [x.name for x in self.planes] if included_planes is None else included_planes
-        exc = [] if excluded_planes is None else excluded_planes
-        inc = filter(lambda x: False if x in exc else True, inc)
-
-        return list(inc)
+        return plan_profitability
 
 
 class Plot(GenericPlot):
@@ -101,59 +87,61 @@ class Plot(GenericPlot):
                 cls.render(xl="Planes", title="Profits vs initial cost " + hub_iata + "-" + dst_iata)
 
     @classmethod
-    def heatmap(cls, data, included_planes=None, excluded_planes=None):
-        heatmap_planes = data.heatmap(included_planes, excluded_planes)
+    def heatmap(cls, data):
+        heatmap_planes = data.heatmap()
+        size = len(data.plannings)
 
-        planes_names = [x.name for x in list(data.planes.values())]
-        planes_ticks = list(filter(lambda s: True if s in included_planes else False, planes_names))
-        for iata, h in data.lines.items():
-            lines_ticks = [iata + "-" + l.dst.iata for l in h.values()]
-            values = []
-            dst_iata = list(h.keys())
-            for i in range(0, len(h)):
-                values.append([])
-                for j in range(0, len(planes_ticks)):
-                    plane = heatmap_planes[iata][h[dst_iata[i]].dst.iata][j]
-                    values[i].append(
-                        plane.profitability(h[dst_iata[i]]) if plane.range > h[dst_iata[i]].distance else 0)
+        plannings_ticks = ["Planning {:d}".format(k + 1) for k in range(0, size)]
 
-            values = np.array(values)
+        lines_ticks = []
+        for hub_iata, lines in data.plannings[0].lines.items():
+            for dst_iata, line in lines.items():
+                lines_ticks.append(hub_iata + "-" + dst_iata)
 
-            # get the tick label font size
-            dpi = 72.27
+        values = []
+        for i in range(0, len(lines_ticks)):
+            hub_iata, dst_iata = lines_ticks[i].split("-")
+            values.append([])
+            for j in range(0, len(plannings_ticks)):
+                values[i].append(heatmap_planes[j][hub_iata][dst_iata])
 
-            # compute the matrix height in points and inches
-            matrix_height_pt = 40 * values.shape[0]
-            matrix_height_in = matrix_height_pt / dpi
+        values = np.array(values)
 
-            # compute the required figure height
-            top_margin = 0.07  # in percentage of the figure height
-            bottom_margin = 0.07  # in percentage of the figure height
-            figure_height = matrix_height_in / (1 - top_margin - bottom_margin)
+        # get the tick label font size
+        dpi = 72.27
 
-            # build the figure instance with the desired height
-            fig, ax = plt.subplots(
-                figsize=(7, figure_height),
-                gridspec_kw=dict(top=1 - top_margin, bottom=bottom_margin))
+        # compute the matrix height in points and inches
+        matrix_height_pt = 40 * values.shape[0]
+        matrix_height_in = matrix_height_pt / dpi
 
-            ax = sns.heatmap(values, ax=ax, cbar=False, cmap="winter")
+        # compute the required figure height
+        top_margin = 0.07  # in percentage of the figure height
+        bottom_margin = 0.07  # in percentage of the figure height
+        figure_height = matrix_height_in / (1 - top_margin - bottom_margin)
 
-            # We want to show all ticks...
-            ax.set_xticks(np.arange(len(planes_ticks)))
-            ax.set_yticks(np.arange(len(lines_ticks)))
-            # ... and label them with the respective list entries
-            ax.set_xticklabels(planes_ticks)
-            ax.set_yticklabels(lines_ticks)
+        # build the figure instance with the desired height
+        fig, ax = plt.subplots(
+            figsize=(7, figure_height),
+            gridspec_kw=dict(top=1 - top_margin, bottom=bottom_margin))
 
-            # Rotate the tick labels and set their alignment.
-            plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+        ax = sns.heatmap(values, ax=ax, cbar=False, cmap="winter")
 
-            plt.setp(ax.get_yticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(len(plannings_ticks)))
+        ax.set_yticks(np.arange(len(lines_ticks)))
+        # ... and label them with the respective list entries
+        ax.set_xticklabels(plannings_ticks)
+        ax.set_yticklabels(lines_ticks)
 
-            # Loop over data dimensions and create text annotations.
-            for i in range(len(lines_ticks)):
-                for j in range(len(included_planes)):
-                    ax.text(j + 0.5, i + 0.5, "{:d} %".format(int(100 * values[i, j])), ha="center", va="center",
-                            color="w")
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 
-            cls.render(title="Profitability comparison", legend=False)
+        plt.setp(ax.get_yticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(lines_ticks)):
+            for j in range(len(plannings_ticks)):
+                ax.text(j + 0.5, i + 0.5, "{:d} %".format(int(100 * values[i, j])), ha="center", va="center",
+                        color="w")
+
+        cls.render(title="Profitability comparison", legend=False)
